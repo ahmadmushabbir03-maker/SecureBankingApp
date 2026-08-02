c1a757ba3b9a5b4cc5799f5b20bfd51ffe6c8316
Ahmad Mushabbir <ahmadmushabbir03@gmail.com>
2026-08-02 08:42:53 +0530

    Phase 3.5 - Accounts page

--- frontend/src/pages/Accounts.jsx
+++ frontend/src/pages/Accounts.jsx
@@ -1,5 +1,113 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  Button,
+  Chip
+} from "@mui/material";
+
 export default function Accounts() {
-    return (
-        <h1>Accounts</h1>
-    );
+
+  const accounts = [
+    {
+      type: "Checking Account",
+      number: "**** 4567",
+      balance: "CAD 5,250.40",
+      status: "Active"
+    },
+    {
+      type: "Savings Account",
+      number: "**** 7890",
+      balance: "CAD 12,800.00",
+      status: "Active"
+    }
+  ];
+
+  return (
+    <>
+
+      <AppBar position="static">
+        <Toolbar>
+
+          <Typography
+            variant="h6"
+            sx={{ flexGrow: 1 }}
+          >
+            Secure Digital Banking
+          </Typography>
+
+          <Button color="inherit">
+            Dashboard
+          </Button>
+
+        </Toolbar>
+      </AppBar>
+
+      <Container sx={{ mt: 4 }}>
+
+        <Typography
+          variant="h4"
+          gutterBottom
+        >
+          My Accounts
+        </Typography>
+
+        <Grid container spacing={3}>
+
+          {accounts.map((account, index) => (
+
+            <Grid size={{ xs: 12, md: 6 }} key={index}>
+
+              <Card elevation={5}>
+
+                <CardContent>
+
+                  <Typography variant="h6">
+                    {account.type}
+                  </Typography>
+
+                  <Typography sx={{ mt: 2 }}>
+                    Account Number
+                  </Typography>
+
+                  <Typography fontWeight="bold">
+                    {account.number}
+                  </Typography>
+
+                  <Typography sx={{ mt: 2 }}>
+                    Available Balance
+                  </Typography>
+
+                  <Typography
+                    variant="h4"
+                    color="primary"
+                  >
+                    {account.balance}
+                  </Typography>
+
+                  <Chip
+                    sx={{ mt: 2 }}
+                    color="success"
+                    label={account.status}
+                  />
+
+                </CardContent>
+
+              </Card>
+
+            </Grid>
+
+          ))}
+
+        </Grid>
+
+      </Container>
+
+    </>
+  );
+
 }
