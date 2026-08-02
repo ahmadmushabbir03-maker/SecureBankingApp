e72bdddf31e37f029f892f79f3109815e26c4500
Ahmad Mushabbir <ahmadmushabbir03@gmail.com>
2026-08-02 08:40:23 +0530

    Phase 3.3 - Professional login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,78 @@
+import {
+  Container,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+  Box
+} from "@mui/material";
+
 export default function Login() {
-    return (
-        <h1>Login Page</h1>
-    );
+
+  return (
+
+    <Container maxWidth="sm">
+
+      <Box
+        sx={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+
+        <Paper
+          elevation={8}
+          sx={{
+            padding: 5,
+            width: "100%",
+            borderRadius: 4
+          }}
+        >
+
+          <Typography
+            variant="h4"
+            align="center"
+            gutterBottom
+          >
+            Secure Digital Banking
+          </Typography>
+
+          <Typography
+            align="center"
+            sx={{ mb: 4 }}
+          >
+            Sign in to your account
+          </Typography>
+
+          <TextField
+            fullWidth
+            label="Email"
+            margin="normal"
+          />
+
+          <TextField
+            fullWidth
+            label="Password"
+            type="password"
+            margin="normal"
+          />
+
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{ mt: 3 }}
+          >
+            Login
+          </Button>
+
+        </Paper>
+
+      </Box>
+
+    </Container>
+
+  );
+
 }
