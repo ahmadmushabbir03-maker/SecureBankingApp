77b37b06cc458d652489a0d9831914a94815097b
Ahmad Mushabbir <ahmadmushabbir03@gmail.com>
2026-08-02 08:04:28 +0530

    Phase 2.8 - Add authentication DTOs

---
+++ backend/src/main/java/com/bank/dto/request/RegisterUserRequest.java
@@ -0,0 +1,78 @@
+package com.bank.dto.request;
+
+import com.bank.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public class RegisterUserRequest {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @Pattern(regexp = "^[0-9+\\-() ]{7,20}$")
+    private String phoneNumber;
+
+    private Role role = Role.CUSTOMER;
+
+    public RegisterUserRequest() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
